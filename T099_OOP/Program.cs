2dbd09e7ba1fedcd91b36d5594949eeee38cbda3
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-04-18 22:22:30 +0200

    Override && Virtual

--- T099_OOP/Program.cs
+++ T099_OOP/Program.cs
@@ -5,31 +5,51 @@ pizza.AddIngredient(new Mozzarella());
 
 Console.WriteLine(pizza.Describe());
 
-var cheddar = new Cheddar();
+Cheddar cheddar = new();
 Console.WriteLine(cheddar.PublicMethod());
 //Console.WriteLine(cheddar.ProtectedMethod());
 //Console.WriteLine(cheddar.PrivateMethod());
 
+Ingredient ingredient = new Cheddar();
+Console.WriteLine("Variable of type Ingredient");
+Console.WriteLine(ingredient.Name);
+
+Console.WriteLine("Variable of type Cheddar");
+Console.WriteLine(cheddar.Name);
+
+Console.WriteLine();
+var ingredients = new List<Ingredient>()
+{
+	new Cheddar(),
+	new TomatoSauce(),
+	new Mozzarella()
+};
+foreach (var item in ingredients)
+{
+	Console.WriteLine(item.Name);
+}
 
 public class Pizza
 {
-	private readonly List<Ingredients> _ingredients = [];
+	private readonly List<Ingredient> _ingredients = [];
 
-	public void AddIngredient(Ingredients ingredient) => _ingredients.Add(ingredient);
+	public void AddIngredient(Ingredient ingredient) => _ingredients.Add(ingredient);
 
 	public string Describe() => $"This is a pizza with {string.Join(", ", _ingredients)}.";
 }
 
-public class Ingredients()
+public class Ingredient()
 {
+	public virtual string Name { get; } = "Some ingredient";
 	public string PublicMethod() => "This method is PUBLIC in the Ingredient class.";
 	protected string ProtectedMethod() => "This method is PROTECTED in the Ingredient class.";
 	private string PrivateMethod() => "This method is PRIVATE in the Ingredient class.";
 }
 
-public class Cheddar : Ingredients
+public class Cheddar : Ingredient
 {
-	public string Name => "Cheddar chees";
+	public override string Name => "Cheddar chees";
+	//public string Name => "Cheddar chees";
 	public int AgedForMonths { get; }
 
 	public void UseMethodsFromBaseClass()
@@ -40,14 +60,14 @@ public class Cheddar : Ingredients
 	}
 }
 
-public class TomatoSauce : Ingredients
+public class TomatoSauce : Ingredient
 {
-	public string Name => "TomatoSauce";
+	public override string Name => "TomatoSauce";
 	public int TomatoesIn100Grams { get; }
 }
 
-public class Mozzarella : Ingredients
+public class Mozzarella : Ingredient
 {
-	public string Name => "Mozzarella";
+	public override string Name => "Mozzarella";
 	public bool IsLight { get; }
 }
