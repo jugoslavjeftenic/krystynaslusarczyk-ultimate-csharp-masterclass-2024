5a69da6ab3bda8d9cd801e83ef3af479175cc5d9
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-04-19 12:20:24 +0200

    Virtual methods - practice

---
+++ T104_Practice_VirtualMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿
+var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, -19 };
+bool shallAddPositiveOnly = true;
+
+NumbersSumCalculator calculator = shallAddPositiveOnly ?
+	new PositiveNumbersSumCalculator() :
+	new NumbersSumCalculator();
+
+int sum = calculator.Calculate(numbers);
+Console.WriteLine("Sum is: " + sum);
+
+public class NumbersSumCalculator
+{
+	public int Calculate(List<int> numbers)
+	{
+		int sum = 0;
+		foreach (var number in numbers)
+		{
+			if (ShallBeAdded(number))
+			{
+				sum += number;
+			}
+		}
+		return sum;
+	}
+
+	protected virtual bool ShallBeAdded(int number)
+	{
+		return true;
+	}
+}
+
+public class PositiveNumbersSumCalculator : NumbersSumCalculator
+{
+	protected override bool ShallBeAdded(int number)
+	{
+		return number > 0;
+	}
+}
