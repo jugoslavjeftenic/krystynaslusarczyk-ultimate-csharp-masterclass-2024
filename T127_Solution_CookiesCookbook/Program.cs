b14668c77b2997d1cdeffd0a3b36de1cefffdc33
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-04-29 08:36:16 +0200

    High level design

---
+++ T127_Solution_CookiesCookbook/Program.cs
@@ -0,0 +1,37 @@
+﻿var cookiesRecipesApp = new CookiesRecipesApp();
+cookiesRecipesApp.Run();
+
+public class CookiesRecipesApp
+	(RecipesRepository recipesRepository, RecipesUserInteraction recipesUserInteraction)
+{
+	private readonly RecipesRepository _recipesRepository = recipesRepository;
+	private readonly RecipesUserInteraction _recipesUserInteraction = recipesUserInteraction;
+
+	public void Run()
+	{
+		var allRecipes = _recipesRepository.Read(filePath);
+		_recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+		_recipesUserInteraction.PromptToCreateRecipe();
+
+		var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+
+		if (ingredients.Count > 0)
+		{
+			var recipe = new Recipe(ingredients);
+			allRecipes.Add(recipe);
+			_recipesRepository.Write(filePath, allRecipes);
+
+			_recipesUserInteraction.ShowMessage("Recipe added:");
+			_recipesUserInteraction.ShowMessage(recipe.ToString());
+		}
+		else
+		{
+			_recipesUserInteraction
+				.ShowMessage("No ingredients have been selected. Recipe will not be saved.");
+		}
+
+		_recipesUserInteraction.Exit();
+	}
+}
+
