98d55aa5876a3b8cc9d7ac1ec0caac93a27ae831
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-02 12:17:03 +0200

    try-catch-finally

---
+++ T137_ExceptionAndErrorHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine() ?? "";
+
+try
+{
+	int number = ParseStringToInt(input);
+	Console.WriteLine("String successfully parsed, the result is" + number);
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"An exception was thrown. Exception message: {ex.Message}");
+}
+finally
+{
+	Console.WriteLine("Finally block is being executed.");
+}
+
+int ParseStringToInt(string input)
+{
+	return int.Parse(input);
+}
