f7777b3f1a5cab983c449e6fdf38ac3626a1eb8f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-03 13:19:19 +0200

    StackOwerflowException && Recursive methods.

--- T137_ExceptionAndErrorHandling/Program.cs
+++ T137_ExceptionAndErrorHandling/Program.cs
@@ -1,54 +1,10 @@
-﻿
-var invalidPersonObject = new Person("", -100);
+﻿RecursiveMethod(1);
 
-var numbers = new int[] { 1, 2, 3 };
-var fourthNumber = numbers[3]; // IndexOutOfRangeException
-
-var emptyCollection = new List<int>();
-var firstElement = GetFirstElement(emptyCollection);
-var firstUsingLinq = emptyCollection.First(); // InvalidOperationException
-
-bool has7 = CheckIfContains(7, numbers);
-
-bool CheckIfContains(int value, int[] numbers)
+static void RecursiveMethod(int counter)
 {
-	throw new NotImplementedException();
-}
-
-static int GetFirstElement(IEnumerable<int> numbers)
-{
-	foreach (int number in numbers)
+	Console.WriteLine($"I'm going to call myself. Counter is: {counter}");
+	if (counter < 10)
 	{
-		return number;
-	}
-
-	throw new InvalidOperationException("The collection cannot be empty.");
-}
-
-class Person
-{
-	public string Name { get; }
-
-	public int YearOfBirth { get; }
-
-	public Person(string name, int yearOfBirth)
-	{
-		//if (name is null)
-		//{
-		//	throw new ArgumentNullException("The name cannot be null.");
-		//}
-		ArgumentNullException.ThrowIfNull(name);
-		if (name.Equals(string.Empty))
-		{
-			throw new ArgumentException("The name cannot be empty.");
-		}
-		if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
-		{
-			throw new ArgumentOutOfRangeException
-				("The year of birth must be between 1900 and the current year.");
-		}
-
-		Name = name;
-		YearOfBirth = yearOfBirth;
+		RecursiveMethod(counter + 1);
 	}
 }
