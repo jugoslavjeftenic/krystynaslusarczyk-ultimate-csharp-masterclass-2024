1134bcc748f681fe6059fc66253d67d727b0d96a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-03 13:34:41 +0200

    Precise excetions

--- T137_ExceptionAndErrorHandling/Program.cs
+++ T137_ExceptionAndErrorHandling/Program.cs
@@ -1,10 +1,29 @@
-﻿RecursiveMethod(1);
+﻿IsFirstElementPositive(new int[] { 1, 2, 3 });
 
-static void RecursiveMethod(int counter)
+static int GetFirstElement(IEnumerable<int> numbers)
 {
-	Console.WriteLine($"I'm going to call myself. Counter is: {counter}");
-	if (counter < 10)
+	Console.WriteLine("Finally block is being executed.");
+	foreach (int number in numbers)
 	{
-		RecursiveMethod(counter + 1);
+		return number;
+	}
+
+	throw new InvalidOperationException("The collection cannot be empty.");
+}
+
+static bool IsFirstElementPositive(IEnumerable<int> numbers)
+{
+	try
+	{
+		var firstElement = GetFirstElement(numbers);
+		return firstElement > 0;
+	}
+	catch (InvalidOperationException ex)
+	{
+		throw new InvalidOperationException("The collection is empty!", ex);
+	}
+	catch (NullReferenceException ex)
+	{
+		throw new ArgumentNullException("The collection is null.", ex);
 	}
 }
