17495d1ca88776cc6ba18ad8dbe4bb45589059fa
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-12 12:59:20 +0200

    Sunny day scenario.

---
+++ T159_Solution_GameDataParser/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Enter the name of the file you want to read:");
+var fileName = Console.ReadLine()!;
+
+if (fileName.Equals("1"))
+{
+	fileName = "games.json";
+}
+if (fileName.Equals("2"))
+{
+	fileName = "gamesEmpty.json";
+}
+if (fileName.Equals("3"))
+{
+	fileName = "gamesInvalidFormat.json";
+}
+
+var fileContents = File.ReadAllText(fileName);
+
+var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+
+if (videoGames?.Count > 0)
+{
+	Console.WriteLine();
+	Console.WriteLine("Loaded games are:");
+
+	foreach (var videoGame in videoGames)
+	{
+		Console.WriteLine(videoGame);
+	}
+}
+else
+{
+	Console.WriteLine("No games are present in the input file.");
+}
+
+public class VideoGame
+{
+	public string Title { get; init; } = string.Empty;
+	public int ReleaseYear { get; init; }
+	public decimal Rating { get; init; }
+
+	public override string ToString() => $"{Title}, released in {ReleaseYear}, rating {Rating}";
+}
