59a59ff07e5b897ab42e78306e3f7a7cfa4a31e7
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-15 10:30:43 +0200

    Simplified List

---
+++ T166_GenericTypes/Program.cs
@@ -0,0 +1,63 @@
+﻿var numbers = new SimpleList<int>();
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+numbers.Add(40);
+numbers.Add(50);
+numbers.RemoveAt(2);
+
+var words = new SimpleList<string>();
+words.Add("aaa");
+words.Add("bbb");
+words.Add("ccc");
+
+class SimpleList<T>
+{
+	private T?[] _items = new T[4];
+	private int _size = 0;
+
+	public void Add(T item)
+	{
+		if (_size >= _items.Length)
+		{
+			var newItems = new T?[_items.Length * 2];
+
+			for (int i = 0; i < _items.Length; i++)
+			{
+				newItems[i] = _items[i];
+			}
+
+			_items = newItems;
+		}
+
+		_items[_size] = item;
+		++_size;
+	}
+
+	public void RemoveAt(int index)
+	{
+		if (index < 0 || index >= _size)
+		{
+			throw new IndexOutOfRangeException($"Index {index} is outside the bund of the list.");
+		}
+
+		--_size;
+
+		for (int i = index; i < _size; ++i)
+		{
+			_items[i] = _items[i + 1];
+		}
+
+		_items[_size] = default;
+	}
+
+	public T? GetAtIndex(int index)
+	{
+		if (index < 0 || index >= _size)
+		{
+			throw new IndexOutOfRangeException($"Index {index} is outside the bund of the list.");
+		}
+
+		return _items[index];
+	}
+}
