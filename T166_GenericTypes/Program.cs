c74d1bdac2f42dcdde53bdaed07a2cbdb9595fe4
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-18 12:08:46 +0200

    Generic methods, "typeof" keyword.

--- T166_GenericTypes/Program.cs
+++ T166_GenericTypes/Program.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections;
+﻿// GenericUriParser methods
+var ints = new List<int> { 1, 2, 3 };
+ints.AddToFront(10);
 
-ArrayList ints = new ArrayList { 2, 3, 4, 5 };
-int sum = 0;
-foreach (object number in ints)
+// Generic methods with multiple type of parameters
+var decimals = new List<decimal> { 1.1m, 0.5m, 22.5m, 12m };
+var intsFromDecimals = decimals.ConvertTo<decimal, int>();
+
+var floats = new List<float> { 1.4f, -100.01f };
+var longsFromFloats = floats.ConvertTo<float, long>();
+
+static class ListExtensions
 {
-	sum += (int)number;
+	public static void AddToFront<T>(this List<T> list, T item)
+	{
+		list.Insert(0, item);
+	}
+
+	public static List<TTarget?> ConvertTo<TSource, TTarget>(this List<TSource> input)
+	{
+		var result = new List<TTarget?>();
+
+		foreach (var item in input)
+		{
+			TTarget? itemAfterCasting = (TTarget?)Convert.ChangeType(item, typeof(TTarget));
+			result.Add(itemAfterCasting);
+		}
+
+		return result;
+	}
 }
-
-ArrayList strings = new ArrayList { "a", "b", "c" };
-
-ArrayList variousItems = new ArrayList { 1, false, "abc", new DateTime() };
-object[] objects = new object[] { 1, false, "abc", new DateTime() };
-
