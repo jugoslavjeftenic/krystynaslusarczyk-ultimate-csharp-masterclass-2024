d74e48c74bb8dd80685af780613a89d4891ab1ba
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-19 13:48:12 +0200

    Improving List performance.

--- T166_GenericTypes/Program.cs
+++ T166_GenericTypes/Program.cs
@@ -1,12 +1,23 @@
-﻿//var points = CreateCollectionOfRandomLength<Point>(100);
-var ints = CreateCollectionOfRandomLength<int>(100);
+﻿using System.Diagnostics;
+
+//var points = CreateCollectionOfRandomLength<Point>(100);
+//var ints = CreateCollectionOfRandomLength<int>(100);
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
 var dates = CreateCollectionOfRandomLength<DateTime>(100);
+
+stopwatch.Stop();
+Console.WriteLine($"Execution took {stopwatch.ElapsedMilliseconds} ms.");
+// Result before optimization: ~1700ms.
+// Result after optimization: ~1020ms.
 
 IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T : new()
 {
-	var length = new Random().Next(maxLength + 1);
+	var length = 100000000; // new Random().Next(maxLength + 1);
 
-	var result = new List<T>();
+	//var result = new List<T>();
+	var result = new List<T>(length);
 
 	for (int i = 0; i < length; i++)
 	{
