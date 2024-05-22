0065f8306da675e197406acfae5d75f2def5a473
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-22 02:18:06 +0200

    Func && Actions.

--- T166_GenericTypes/Program.cs
+++ T166_GenericTypes/Program.cs
@@ -1,18 +1,35 @@
-﻿SomeMethod(new Pet(), new PetOwner());
+﻿var numbers = new[] { 1, 4, 7, 19, 2 };
 
-void SomeMethod<TPet, TOwner>(TPet pet, TOwner owner)
-	where TPet : Pet, IComparable<TPet>
-	where TOwner : new()
+// Func<int, DateTime, string, decimal> someFunc; ;
+// Action<string, string, bool> someAction; ;
+
+Func<int, bool> predicate1 = IsLargerThen10;
+Console.WriteLine("IsAnyLargerThen10? " + IsAny(numbers, predicate1));
+Console.WriteLine("IsAnyLargerThen10? " + IsAny(numbers, IsLargerThen10));
+
+Func<int, bool> predicate2 = IsEven;
+Console.WriteLine("IsAnyEvan? " + IsAny(numbers, predicate2));
+Console.WriteLine("IsAnyEvan? " + IsAny(numbers, IsEven));
+
+bool IsLargerThen10(int number)
 {
-
+	return number > 10;
 }
 
-public class Pet : IComparable<Pet>
+bool IsEven(int number)
 {
-	public int CompareTo(Pet? other)
+	return number % 2 == 0;
+}
+
+bool IsAny(IEnumerable<int> numbers, Func<int, bool> predicate)
+{
+	foreach (int number in numbers)
 	{
-		throw new NotImplementedException();
+		if (predicate(number))
+		{
+			return true;
+		}
 	}
-}
 
-public class PetOwner { }
+	return false;
+}
