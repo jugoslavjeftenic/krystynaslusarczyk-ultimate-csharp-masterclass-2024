7a6c64cdee616205d82c727acb4baeea6c407de2
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-05-22 11:39:21 +0200

    Lambda expressions.

--- T166_GenericTypes/Program.cs
+++ T166_GenericTypes/Program.cs
@@ -7,9 +7,15 @@ Func<int, bool> predicate1 = IsLargerThen10;
 Console.WriteLine("IsAnyLargerThen10? " + IsAny(numbers, predicate1));
 Console.WriteLine("IsAnyLargerThen10? " + IsAny(numbers, IsLargerThen10));
 
+// Lambda expression
+Console.WriteLine("IsAnyLargerThen10? " + IsAny(numbers, n => n > 10));
+
 Func<int, bool> predicate2 = IsEven;
 Console.WriteLine("IsAnyEvan? " + IsAny(numbers, predicate2));
 Console.WriteLine("IsAnyEvan? " + IsAny(numbers, IsEven));
+
+// Lambda expression
+Console.WriteLine("IsAnyEvan? " + IsAny(numbers, n => n % 2 == 0));
 
 bool IsLargerThen10(int number)
 {
