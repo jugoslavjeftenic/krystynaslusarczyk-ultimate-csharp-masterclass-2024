edea9a332ff17133b4ad50eb23c1f51363df35e1
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-06-09 15:16:11 +0200

    Generic filtering of collections.

--- T166_GenericTypes/Program.cs
+++ T166_GenericTypes/Program.cs
@@ -1,24 +1,30 @@
 ﻿
 var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+var filteringStrategySelector = new FilteringStrategySelector();
 
-Console.WriteLine("Select filter: Even, Odd, Positive");
+Console.WriteLine("Select filter:");
+Console.WriteLine(string.Join(
+	Environment.NewLine, filteringStrategySelector.FilteringStrategiesNames));
 var userInput = Console.ReadLine() ?? "";
 
 var filteringStrategy = new FilteringStrategySelector().Select(userInput);
-List<int> result = new NumbersFilter().FilterBy(filteringStrategy, numbers);
+var result = new Filter().FilterBy(filteringStrategy, numbers);
 
 Print(result);
+
+var words = new[] { "zebra", "ostrich", "otter" };
+var oWords = new Filter().FilterBy(word => word.StartsWith("o"), words);
 
 void Print(IEnumerable<int> numbers)
 {
 	Console.WriteLine(string.Join(", ", numbers));
 }
 
-public class NumbersFilter
+public class Filter
 {
-	public List<int> FilterBy(Func<int, bool> predicate, List<int> numbers)
+	public IEnumerable<T> FilterBy<T>(Func<T, bool> predicate, IEnumerable<T> numbers)
 	{
-		var result = new List<int>();
+		var result = new List<T>();
 
 		foreach (var number in numbers)
 		{
@@ -29,6 +35,7 @@ public class NumbersFilter
 	}
 
 }
+
 public class FilteringStrategySelector
 {
 	private readonly Dictionary<string, Func<int, bool>> _filteringStrategies =
@@ -36,8 +43,11 @@ public class FilteringStrategySelector
 		{
 			["Even"] = number => number % 2 == 0,
 			["Odd"] = number => number % 2 == 1,
-			["Positive"] = number => number > 0
+			["Positive"] = number => number > 0,
+			["Negative"] = number => number < 0,
 		};
+
+	public IEnumerable<string> FilteringStrategiesNames => _filteringStrategies.Keys;
 
 	public Func<int, bool> Select(string filteringType)
 	{
