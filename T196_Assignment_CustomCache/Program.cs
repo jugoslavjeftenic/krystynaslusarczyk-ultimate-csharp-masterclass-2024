b6fa3baf5a457385a9f55d1c8197acdf716513d4
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-06-11 11:12:40 +0200

    Initial setup.

---
+++ T196_Assignment_CustomCache/Program.cs
@@ -0,0 +1,23 @@
+﻿var dataDownloader = new SlowDataDownloader();
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id4"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+public interface IDataDownloader
+{
+	string DownloadData(string resourceId);
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+	public string DownloadData(string resourceId)
+	{
+		Thread.Sleep(1000);
+		return $"Some data for {resourceId}";
+	}
+}
